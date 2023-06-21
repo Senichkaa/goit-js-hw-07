eaf263407a84c7674566502f598d2938e6c6f5b3
Senichkaa <super-kapanaiko406@ukr.net>
2023-06-21 23:58:46 +0300

    1st h.a

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+const createGallery = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+};
+
+const mapGallery = galleryItems.map(createGallery).join("");
+gallery.innerHTML = mapGallery;
+
+gallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
+
+  instance.show();
+}
 console.log(galleryItems);
