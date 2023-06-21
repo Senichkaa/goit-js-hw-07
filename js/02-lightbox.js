9cb01ef0cb76486e940cd4bd20493c5a2dc0c9d1
Senichkaa <super-kapanaiko406@ukr.net>
2023-06-22 00:56:20 +0300

    2nd h.a

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+};
+
+const mapGallery = galleryItems.map(createGallery).join("");
+gallery.innerHTML = mapGallery;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captoinDelay: 250,
+});
 
 console.log(galleryItems);
